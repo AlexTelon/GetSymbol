c6c23b26d8c5c3adebc3402053d5ff9cb15ff4ce
Alex Telon <alex.telon@gmail.com>
2023-03-02 21:58:54 +0100

    Renamed stuff

--- getutf/openai_utils.py
+++ getutf/openai_utils.py
@@ -1,24 +1,8 @@
 import openai
 
-# Read the OpenAI API key from a hidden local file
 with open('.openai_key.secret', 'r') as f:
     openai.api_key = f.read().strip()
 
-# Input: arrows
-# Example output:
-# ⇐ ⇑ ⇒ ⇓ ⇔ ⇕ ⇖ ⇗ ⇘ ⇙ ⇚ ⇛ ⇜ ⇝ ⇞ ⇟
-
-# Input: degrees
-# Example
-# ° ℃ ℉
-
-# Input: letters with dots over them
-# Example:
-# ̇a ̇b ̇c ḍe ḥf ḳg ḷh ṃi ṇj ṛk ṣl ṭm ̇n ̇o ̇p ̇q ṡr ṭu ṿw ẋx ẏy ̣z
-# ̇A ̇B ḌC ḤD ḲE ḶF ṂG ṆH ṚI ṢJ ṬK ̇L ̇M ̇N ̇O ̇P ̇Q ṢR ṬU ṾV ẊW ẎX ẒY
-
-
-# Define the standard prompt header
 PROMPT_HEADER = """
 You are a cli tool that given an input you answer with matching utf chars only.
 No text or explanations are allowed.
@@ -26,12 +10,13 @@ No text or explanations are allowed.
 input:"""
 
 def get_chars(prompt):
-
     prompt=f"{PROMPT_HEADER}{prompt}"
-    print(prompt)
+    engine = "text-davinci-003" # works well but most expensive
+    engine = 'text-davinci-002' # also seems to work well and might be cheaper?
+    # the other engines were bad at least with this current prompt and temperature etc.
 
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine=engine,
         prompt=prompt,
         max_tokens=100,
         n=1,
@@ -39,5 +24,4 @@ def get_chars(prompt):
         temperature=1,
     )
 
-    # Return the response text as a string
     return response.choices[0].text.strip()
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='getutf',
+    name='GetSymbol',
     version='0.1',
-    packages=['getutf'],
+    packages=['getsymbol'],
     entry_points={
         'console_scripts': [
-            'getutf = getutf.main:main'
+            'getsymbol = getsymbol.main:main'
         ]
     },
     author='Alex Telon',
     author_email='alex.telon@gmail.com',
     description='A tool to get the utf char from freetext english description using AI',
-    url='https://github.com/AlexTelon/getutf',
+    url='https://github.com/AlexTelon/getsymbol',
     license='MIT'
 )
