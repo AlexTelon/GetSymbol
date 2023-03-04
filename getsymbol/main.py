4bfc400d7824e7a20aacf94a1f93c6a5c6f41bba
Alex Telon <alex.telon@gmail.com>
2023-03-04 22:00:38 +0100

    Added a get_symbol api
    Also modified the prompt with some trial and error.
    
    Having the prompt end with something like "input:" is important to make
    sure the model never tries to complete the prompt header.
    
    top_p instead of max_tokens together with an added instruction to only
    return one symbol worked well.

--- getsymbol/main.py
+++ getsymbol/main.py
@@ -1,12 +1,13 @@
 import sys
 
-from .openai_utils import get_chars
+from .openai_utils import get_symbols, get_symbol
 
 def main():
     prompt = ' '.join(sys.argv[1:])
     if prompt == '':
         return
-    print(get_chars(prompt))
+    print(get_symbols(prompt))
+    # print(get_symbol(prompt))
 
 if __name__ == '__main__':
     main()
--- getsymbol/openai_utils.py
+++ getsymbol/openai_utils.py
@@ -11,25 +11,40 @@ else:
     with open(api_key_path, 'r') as f:
         openai.api_key = f.read().strip()
 
-PROMPT_HEADER = """
-You are a cli tool that given an input you answer with matching utf chars only.
-No text or explanations are allowed.
 
-input:"""
+PROMPT_HEADER = """You are a cli tool that given an input you answer with matching utf chars only.
+No text or explanations are allowed. Only unicode symbols. No english words or letters in your response.
+Respond with one or more symbols seperated by space only.
+"""
 
-def get_chars(prompt):
-    prompt=f"{PROMPT_HEADER}{prompt}"
-    engine = "text-davinci-003" # works well but most expensive
-    engine = 'text-davinci-002' # also seems to work well and might be cheaper?
+def get_symbols(prompt: str) -> str:
+    """Given a prompt, return matching symbols."""
+    # engine = "text-davinci-003" # works well but most expensive
+    engine = 'text-davinci-003' # also seems to work well and might be cheaper?
     # the other engines were bad at least with this current prompt and temperature etc.
 
     response = openai.Completion.create(
         engine=engine,
-        prompt=prompt,
+        prompt=f"{PROMPT_HEADER}\nInput:{prompt}",
         max_tokens=100,
         n=1,
         stop=None,
         temperature=1,
+    )
+
+    return response.choices[0].text.strip()
+
+def get_symbol(prompt: str) -> str:
+    """Given a prompt, return one matching symbol."""
+    engine = 'text-davinci-003'
+
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=f"{PROMPT_HEADER}.Only return one symbol.\nInput:{prompt}",
+        max_tokens=40,
+        n=1,
+        stop=None,
+        top_p=0.01,
     )
 
     return response.choices[0].text.strip()
